6521da57cf5b2b62eab95000ced4099817212cdd
supunAkalanka96 <sakalanka713@gmail.com>
2023-10-07 15:55:00 +0530

    code

--- App.js
+++ App.js
@@ -1,11 +1,79 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
+export default function App()
+{
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() =>
+  {
+    (async () =>
+    {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted')
+      {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  const sendLocationDataToServer = async (locationData) =>
+  {
+    try
+    {
+      const response = await axios.post(' https://b3c6-2402-4000-b280-6332-8845-de9b-6c81-ac47.ngrok-free.app /location/save', locationData);
+      console.log('Location data sent successfully:', response.data);
+    } catch (error)
+    {
+      console.error('Error sending location data:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Current Location:</Text>
+      {errorMsg ? <Text>{errorMsg}</Text> : null}
+      {location ? (
+        <Text>
+          Latitude: {location.coords.latitude}, Longitude: {location.coords.longitude}
+        </Text>
+      ) : null}
+      <Button
+        title="Get Location"
+        onPress={() =>
+        {
+          setLocation(null);
+          setErrorMsg(null);
+          (async () =>
+          {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted')
+            {
+              setErrorMsg('Permission to access location was denied');
+              return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+
+            if (location)
+            {
+              const locationData = {
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              };
+              sendLocationDataToServer(locationData);
+            }
+          })();
+        }}
+      />
     </View>
   );
 }
@@ -13,7 +81,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ assets/road_sign_description/decrpition.js
@@ -0,0 +1,207 @@
+const array = [
+    {
+        "image": "/assets/road_sign/(1).jpeg",
+        "NameOfSign": "No Parking and Standing",
+    },
+    {
+        "image": "/assets/road_sign/(2).jpeg",
+        "NameOfSign": "Do not overtake",
+    },
+    {
+        "image": "/assets/road_sign/(3).jpeg",
+        "NameOfSign": "Railway Level Crossing Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(4).jpeg",
+        "NameOfSign": " Speed Limit for 3 wheelers and land vehicles in built-up and non built-up areas",
+    },
+    {
+        "image": "/assets/road_sign/(5).jpeg",
+        "NameOfSign": "Road Narrows Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(6).jpeg",
+        "NameOfSign": "No Horning",
+    },
+    {
+        "image": "/assets/road_sign/(7).jpeg",
+        "NameOfSign": "Priority Road to vehicles on the major road",
+    },
+    {
+        "image": "/assets/road_sign/(8).jpeg",
+        "NameOfSign": " U-turn",
+    },
+    {
+        "image": "/assets/road_sign/(9).jpeg",
+        "NameOfSign": "Yellow Light",
+    },
+    {
+        "image": "/assets/road_sign/(10).jpeg",
+        "NameOfSign": "Traffic From Left Merges Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(11).jpeg",
+        "NameOfSign": "Red Light",
+    },
+    {
+        "image": "/assets/road_sign/(12).jpeg",
+        "NameOfSign": "Dual Carriage-way Ends",
+    },
+    {
+        "image": "/assets/road_sign/(13).jpeg",
+        "NameOfSign": "Dual Carriage-way Starts Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(14).jpeg",
+        "NameOfSign": "Give Way to vehicles on the adjacent road",
+    },
+    {
+        "image": "/assets/road_sign/(15).jpeg",
+        "NameOfSign": "Complex Road",
+    },
+    {
+        "image": "/assets/road_sign/(16).jpeg",
+        "NameOfSign": "Turn left only",
+    },
+    {
+        "image": "/assets/road_sign/(17).jpeg",
+        "NameOfSign": "Unprotected Level Crossing Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(18).jpeg",
+        "NameOfSign": "No Parking",
+    },
+    {
+        "image": "/assets/road_sign/(19).jpeg",
+        "NameOfSign": "End of Priority given to vehicles on the major road",
+    },
+    {
+        "image": "/assets/road_sign/(20).jpeg",
+        "NameOfSign": "Road Closed for all vehicles",
+    },
+    {
+        "image": "/assets/road_sign/(21).jpeg",
+        "NameOfSign": "Priority over Oncoming Traffic Vehicles",
+    },
+    {
+        "image": "/assets/road_sign/(22).jpeg",
+        "NameOfSign": "Pedestrian Crossing Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(23).jpeg",
+        "NameOfSign": "No Entry",
+    },
+    {
+        "image": "/assets/road_sign/(24).jpeg",
+        "NameOfSign": "Narrow Bridge or Culvert Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(25).jpeg",
+        "NameOfSign": "No Parking",
+    },
+    {
+        "image": "/assets/road_sign/(26).jpeg",
+        "NameOfSign": "Falling Rocks Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(27).jpeg",
+        "NameOfSign": "No Parking and Standing",
+    },
+    {
+        "image": "/assets/road_sign/(28).jpeg",
+        "NameOfSign": "Give Way Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(29).jpeg",
+        "NameOfSign": "Road closed for Motor vehicles",
+    },
+    {
+        "image": "/assets/road_sign/(30).jpeg",
+        "NameOfSign": "Turn Left Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(31).jpeg",
+        "NameOfSign": "Stop Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(32).jpeg",
+        "NameOfSign": "Slippery Road Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(33).jpeg",
+        "NameOfSign": "Dangerous Descent Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(34).jpeg",
+        "NameOfSign": "Pedestrian Crossing Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(35).jpeg",
+        "NameOfSign": "End of Speed Limit",
+    },
+    {
+        "image": "/assets/road_sign/(36).jpeg",
+        "NameOfSign": "No Parking",
+    },
+    {
+        "image": "/assets/road_sign/(37).jpeg",
+        "NameOfSign": "Right Bend Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(38).jpeg",
+        "NameOfSign": "Weight Limit",
+    },
+    {
+        "image": "/assets/road_sign/(39).jpeg",
+        "NameOfSign": "Parking",
+    },
+    {
+        "image": "/assets/road_sign/(40).jpeg",
+        "NameOfSign": "Children present/ crossing Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(41).jpeg",
+        "NameOfSign": "Roundabout Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(42).jpeg",
+        "NameOfSign": "Go Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(43).jpeg",
+        "NameOfSign": "Do not Entry",
+    },
+    {
+        "image": "/assets/road_sign/(44).jpeg",
+        "NameOfSign": "Double Lane",
+    },
+    {
+        "image": "/assets/road_sign/(45).jpeg",
+        "NameOfSign": "Single Lane",
+    },
+    {
+        "image": "/assets/road_sign/(46).jpeg",
+        "NameOfSign": "ABC",
+    },
+    {
+        "image": "/assets/road_sign/(47).jpeg",
+        "NameOfSign": "Pass Left Side",
+    },
+    {
+        "image": "/assets/road_sign/(48).jpeg",
+        "NameOfSign": "Turn Right Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(49).jpeg",
+        "NameOfSign": "Left Bend Ahead",
+    },
+    {
+        "image": "/assets/road_sign/(50).jpeg",
+        "NameOfSign": "U Turn",
+    },
+    {
+        "image": "/assets/road_sign/(51).jpeg",
+        "NameOfSign": "DO not Go There",
+    },
+
+]
